d666c87ee4ed1c6b5d343f756c07f4a34dfab866
qcfu <qcfu@bu.edu>
2024-02-07 12:55:12 -0500

    lab04 tuple sorting

---
+++ labs/lab04/lab04_04.ml
@@ -0,0 +1,12 @@
+(* Sorting Tuples 
+
+   Given a tuple of integers of size 5, sort all of its elements
+   WITHOUT first converting it into a list or array. *)
+
+type tup2 = int * int
+type tup3 = int * int * int
+type tup4 = int * int * int * int
+type tup5 = int * int * int * int * int
+
+let sort (xs : tup5): tup5 = 
+  assert false
